c16ecc88c93df489628abcbd57a591b55698c6df
Santiago Encina <santiago.encina@ing.asutral.edu.ar>
2023-11-05 12:34:56 -0300

    hu moments

--- tphu/huFile.py
+++ tphu/huFile.py
@@ -2,7 +2,8 @@ import os
 import cv2 as cv
 import pandas as pd
 
-SHAPES_FOLDER_PATH = os.path.join(os.path.dirname(__file__), '../tp1')
+# Navigate backward one directory to access 'tp1'
+SHAPES_FOLDER_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'shapes'))
 
 def calculate_hu_moments(image):
     # Calculate Hu moments
@@ -24,31 +25,33 @@ def process_images_folder():
     # Initialize an empty list to store the results
     hu_moments_data = []
 
-    # Iterate over subfolders (classes)
+    # Iterate over subdirectories (classes) in 'shapes'
     for label, class_folder in enumerate(os.listdir(SHAPES_FOLDER_PATH), start=1):
         class_folder_path = os.path.join(SHAPES_FOLDER_PATH, class_folder)
 
-        # Iterate over images in the class folder
-        for filename in os.listdir(class_folder_path):
-            if filename.endswith(".png"):
-                image_path = os.path.join(class_folder_path, filename)
-                data = process_image(image_path, label)
-                hu_moments_data.append(data)
+        # Ensure class_folder_path is a directory
+        if os.path.isdir(class_folder_path):
+            # Iterate over images in the class folder
+            for filename in os.listdir(class_folder_path):
+                if filename.endswith(".png"):
+                    image_path = os.path.join(class_folder_path, filename)
+                    data = process_image(image_path, label)
+                    hu_moments_data.append(data)
 
     # Create a DataFrame from the list
     hu_moments_df = pd.DataFrame(hu_moments_data)
 
-    # Specify the output folder (archivos) within the project folder (tp2)
-    output_folder = os.path.join(os.path.dirname(__file__), 'archivos')
+    # Specify the output folder (data) within the project folder (tp2)
+    output_folder = os.path.join(os.path.dirname(__file__), 'data')
 
     # Create the output folder if it doesn't exist
     os.makedirs(output_folder, exist_ok=True)
 
-    # Save the DataFrame to a CSV file inside the archivos folder
+    # Save the DataFrame to a CSV file inside the data folder
     output_path = os.path.join(output_folder, 'extra_huMoments.csv')
     hu_moments_df.to_csv(output_path, index=False)
 
-    print("HuMoments have been saved to the extra_huMoments.csv in archivos folder.")
+    print("HuMoments have been saved to the extra_huMoments.csv in data folder.")
 
 if __name__ == "__main__":
     process_images_folder()
